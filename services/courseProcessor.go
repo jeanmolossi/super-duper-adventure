dc9f50f0781b936ad0e7513c8463d512a82677e6
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-01 01:49:15 -0300

    refactor: alterado processador de cursos

--- services/courseProcessor.go
+++ services/courseProcessor.go
@@ -1,46 +1,47 @@
 package services
 
 import (
+	"github.com/jeanmolossi/super-duper-adventure/domain"
 	"github.com/jeanmolossi/super-duper-adventure/queue"
 	"github.com/streadway/amqp"
 	"log"
 )
 
 type CourseProcessor struct {
-	MessageChannel chan amqp.Delivery
-	RabbitMQ       *queue.RabbitMQ
+	MessageChannel  chan amqp.Delivery
+	RabbitMQ        *queue.RabbitMQ
+	StudentsChannel chan ProcessedCourseResult
 }
 
-func NewCourseProcessor(messageChannel chan amqp.Delivery, rabbitMQ *queue.RabbitMQ) *CourseProcessor {
+func NewCourseProcessor(messageChannel chan amqp.Delivery, rabbitMQ *queue.RabbitMQ, studentsChannel chan ProcessedCourseResult) *CourseProcessor {
 	return &CourseProcessor{
-		MessageChannel: messageChannel,
-		RabbitMQ:       rabbitMQ,
+		MessageChannel:  messageChannel,
+		RabbitMQ:        rabbitMQ,
+		StudentsChannel: studentsChannel,
 	}
 }
 
 func (c *CourseProcessor) Start(channel *amqp.Channel) {
-	workReturnChannel := make(chan string)
-
-	for processes := 0; processes < 2; processes++ {
-		go WorkOn(c.MessageChannel, workReturnChannel)
+	for processes := 0; processes < 10; processes++ {
+		go WorkOn(c.MessageChannel, c.StudentsChannel)
 	}
-
-	for processWork := range workReturnChannel {
-		log.Printf("Process status: %s", processWork)
-	}
-
 }
 
-func WorkOn(messageChannel chan amqp.Delivery, workReturnChannel chan string) {
-	for course := range messageChannel {
-		log.Printf("Curso: %s", string(course.Body))
+func WorkOn(messageChannel chan amqp.Delivery, studentsProcessorChannel chan ProcessedCourseResult) {
+	for courseMessage := range messageChannel {
+		log.Printf("Processando curso: %s", string(courseMessage.Body))
 
-		err := course.Ack(false)
+		courseInstance := domain.NewCourse()
+		course, err := courseInstance.GetCourse(string(courseMessage.Body))
 		if err != nil {
-			workReturnChannel <- "end"
-			return
+			log.Printf("Mensagem rejeitada: %s", err.Error())
+			courseMessage.Reject(false)
+			continue
 		}
 
-		workReturnChannel <- "end"
+		studentsProcessorChannel <- ProcessedCourseResult{
+			Course:  *course,
+			Message: courseMessage,
+		}
 	}
 }
