cc6d0d75bf03680b2d8304368a3dc8e156d23d2c
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-10-03 23:37:55 -0300

    refactor: serviço de processamento de curso melhorado

--- services/courseProcessor.go
+++ services/courseProcessor.go
@@ -2,46 +2,107 @@ package services
 
 import (
 	"github.com/jeanmolossi/super-duper-adventure/domain"
-	"github.com/jeanmolossi/super-duper-adventure/queue"
 	"github.com/streadway/amqp"
 	"log"
+	"time"
 )
 
+type ProcessedCourseResult struct {
+	Course  domain.Course
+	Message amqp.Delivery
+	Error   error
+}
+
 type CourseProcessor struct {
-	MessageChannel  chan amqp.Delivery
-	RabbitMQ        *queue.RabbitMQ
-	StudentsChannel chan ProcessedCourseResult
+	MessageChannel chan amqp.Delivery
+	ReturnChannel  chan ProcessedCourseResult
 }
 
-func NewCourseProcessor(messageChannel chan amqp.Delivery, rabbitMQ *queue.RabbitMQ, studentsChannel chan ProcessedCourseResult) *CourseProcessor {
+func NewCourseProcessor(messageChannel chan amqp.Delivery, returnChannel chan ProcessedCourseResult) *CourseProcessor {
 	return &CourseProcessor{
-		MessageChannel:  messageChannel,
-		RabbitMQ:        rabbitMQ,
-		StudentsChannel: studentsChannel,
+		MessageChannel: messageChannel,
+		ReturnChannel:  returnChannel,
 	}
 }
 
-func (c *CourseProcessor) Start(channel *amqp.Channel) {
-	for processes := 0; processes < 10; processes++ {
-		go WorkOn(c.MessageChannel, c.StudentsChannel)
-	}
-}
-
-func WorkOn(messageChannel chan amqp.Delivery, studentsProcessorChannel chan ProcessedCourseResult) {
-	for courseMessage := range messageChannel {
+func (c *CourseProcessor) Start() {
+	for courseMessage := range c.MessageChannel {
+		startedProcess := time.Now()
 		log.Printf("Processando curso: %s", string(courseMessage.Body))
 
-		courseInstance := domain.NewCourse()
-		course, err := courseInstance.GetCourse(string(courseMessage.Body))
-		if err != nil {
-			log.Printf("Mensagem rejeitada: %s", err.Error())
-			courseMessage.Reject(false)
-			continue
-		}
+		course := domain.NewCourse()
+		err := course.GetCourse(string(courseMessage.Body))
 
-		studentsProcessorChannel <- ProcessedCourseResult{
+		processedResult := ProcessedCourseResult{
 			Course:  *course,
 			Message: courseMessage,
+			Error:   err,
 		}
+
+		if err != nil {
+			log.Printf("Mensagem rejeitada: %s", err.Error())
+			err := courseMessage.Reject(false)
+			if err != nil {
+				return
+			}
+
+			processedResult.Error = err
+			c.ReturnChannel <- processedResult
+
+			return
+		}
+
+		students, err := course.GetStudents()
+		if err != nil {
+			log.Printf("Mensagem rejeitada: %s", err.Error())
+			err := courseMessage.Reject(true)
+			if err != nil {
+				return
+			}
+
+			processedResult.Error = err
+			c.ReturnChannel <- processedResult
+
+			return
+		}
+
+		log.Printf("Antes de processar students do curso %s: %s", course.ID, time.Since(startedProcess))
+
+		err = c.ProcessStudents(students)
+		if err != nil {
+			log.Printf("Processamento de estudantes com erro: %s", err.Error())
+
+			mustRequeue := err.Error() != "Curso já existe no redis"
+			err := courseMessage.Reject(mustRequeue)
+			if err != nil {
+				return
+			}
+
+			processedResult.Error = err
+			c.ReturnChannel <- processedResult
+
+			return
+		}
+
+		log.Printf(
+			"Processamento %s terminado em %v. (Total %d alunos)\n",
+			processedResult.Course.ID,
+			time.Since(startedProcess),
+			len(students),
+		)
+
+		err = courseMessage.Ack(false)
+		if err != nil {
+			return
+		}
+		c.ReturnChannel <- processedResult
 	}
+}
+
+func (c *CourseProcessor) ProcessStudents(students []domain.Student) error {
+	studentsProcessor := NewStudentProcessor()
+	studentsProcessor.Students = students
+	err := studentsProcessor.Start()
+
+	return err
 }
